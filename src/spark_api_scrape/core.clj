f05b90e9351ddb1a23f26ce66052f235d79131d2
wronsiek <warrenronsiek@gmail.com>
2020-07-26 11:13:34 -0400

    first commit

---
+++ src/spark_api_scrape/core.clj
@@ -0,0 +1,6 @@
+(ns spark-api-scrape.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/spark_api_scrape/core_test.clj
@@ -0,0 +1,7 @@
+(ns spark-api-scrape.core-test
+  (:require [clojure.test :refer :all]
+            [spark-api-scrape.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
